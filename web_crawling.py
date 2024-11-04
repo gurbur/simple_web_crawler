48c68545cca2a8af0307a6c95f68fff110223ae1
gurbur <jihwan7577@gmail.com>
2024-11-04 22:39:23 +0900

    Feat: now with saving

--- web_crawling.py
+++ web_crawling.py
@@ -4,11 +4,12 @@ import pandas as pd
 
 df = pd.read_csv("movies.csv")
 
+data = {'movieId': [], 'director': []}
+
 specific_column = df['title']
 
-print(specific_column)
-
 for i in range(1, 10):
+    print(i, "ongoing...")
     movie_name = specific_column[i]
 
     url = "https://www.imdb.com/find/?q={}&ref_=nv_sr_sm".format(movie_name)
@@ -20,11 +21,15 @@ for i in range(1, 10):
     else:
         print("Error! failed to bring page");
 
-    print("Brought successful:")
-
     soup = BeautifulSoup(html, "html.parser")
-    # 가져온 html에서 첫 번째 제목부분 찾아서 가져오기
+    # 가져온 html에서 첫 번째 감독, 배우 부분 찾아서 가져오기
     info = soup.find("ul", class_="ipc-metadata-list-summary-item__stl").get_text()
 
-    print(info)
-    
+    info = info.split(",")[0]
+
+    data['movieId'].append(i)
+    data['director'].append(info)
+
+output_df = pd.DataFrame(data)
+
+output_df.to_csv('output.csv', index=False)
